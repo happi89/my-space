f5e651de6579868300031b50aba393a388bcc18a
Farhaan Patel <farhaanp99@gmail.com>
2023-06-05 09:30:27 -0400

    removed auto redirect to about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-
-	throw redirect('/about')
 
 	return (
 		<div className='mt-[-8rem] flex-grow w-full min-h-screen flex justify-center items-center'>
